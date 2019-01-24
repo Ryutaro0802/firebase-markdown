d6bda3577a833e92ed694a7b7a1c0185522cd46a
Ryutaro Kawakami <ne200152g@gmail.com>
2019-01-24 16:20:58 +0900

    webpack.configを修正

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: './dist',
+    publicPath: '/',
+    // publicPath: '/dist',
     filename: 'build.js'
   },
   module: {
